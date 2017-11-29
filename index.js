381278042ede642594a88b30d9c216e2e4676c97
Anish George <anish.george@ignitesol.com>
2017-11-29 09:57:55 +0530

    Add commandline args and configuration

---
+++ index.js
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////////////////////////////////////
+//                  Main runner for our notification system                  //
+///////////////////////////////////////////////////////////////////////////////
+
+var argv = require('minimist')(process.argv.slice(2));
+var fs = require('fs');
+
+var configFile = "config.json";
+
+if (argv.configure) {
+    console.log("We are getting ready to configure this tool");
+    process.exit(0);
+}
+
+
+console.dir(argv);
+
+try {
+    var config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+} catch (err) {
+    console.log("Bad configuration file");
+    console.log(err);
+    process.exit(1);
+}
+
+console.log("We have loaded the configuration");
+console.dir(config);
